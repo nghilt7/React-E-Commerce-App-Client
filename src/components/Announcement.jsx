2e868f1db59862c05b86e5fb69b6f82e59f716db
nghilt7 <nghilt2307@gmail.com>
2023-04-02 19:27:19 +0700

    Add Navbar

---
+++ src/components/Announcement.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: #fff;
+`;
+
+const Announcement = () => {
+  return <Container>Super Deal! Free Shipping on Orders Over $50</Container>;
+};
+
+export default Announcement;
