0982892d896195f4a3fecf33b59ec0673fbf5b18
nghilt7 <nghilt2307@gmail.com>
2023-04-02 20:54:01 +0700

    Add Slider, Announcement

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -4,6 +4,11 @@ const Container = styled.div`
   height: 30px;
   background-color: teal;
   color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  font-weight: 500;
 `;
 
 const Announcement = () => {
